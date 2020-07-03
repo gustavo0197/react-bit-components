988560289a256c974b75038c5a4e9161a843c215
Gustavo <gustavo.velazquezm@outlook.com>
2020-07-03 00:33:02 -0500

    Button component

--- dist/Button.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import "./button.scss";
-export declare const Button: ({ onClick, text, className }: ButtonProps) => JSX.Element;
-interface ButtonProps {
-    onClick: () => any;
-    text: string;
-    className?: string;
-}
-export {};
--- dist/Button.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import "./button.scss";
-export const Button = ({ onClick, text, className }) => {
-    return (React.createElement("button", { className: `btn ${className}`, onClick: onClick }, text));
-};
